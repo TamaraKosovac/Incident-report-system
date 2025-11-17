ee74f699c10a50a4691b83816c01f0b1f03ac2d7
TamaraKosovac <tamarakosovac14@gmail.com>
2025-11-17 21:11:36 +0100

    Discovery-service

---
+++ IncidentTracker/backend/discovery-service/src/main/java/org/unibl/etf/pisio/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package org.unibl.etf.pisio.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
